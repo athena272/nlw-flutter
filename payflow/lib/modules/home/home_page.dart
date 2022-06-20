3e9ad5f2c34b6643930ab3b76e9636b171e7fd3e
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-06-20 16:16:42 -0300

    Update home_page.dart

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +12,29 @@ class HomePage extends StatelessWidget {
         backgroundColor: AppColors.primary,
         title: Text("HomePage"),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("Welcome to HomePage", textAlign: TextAlign.center),
+          TextButton(
+              onPressed: () async {
+                bool leave = await logout();
+                if (leave) {
+                  Navigator.pushReplacementNamed(context, "/splash");
+                }
+              },
+              child: Text("Logout"))
+        ],
+      )
     );
+  }
+
+  Future<bool> logout() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    await sharedPreferences.clear();
+    
+    return true;
   }
 }
