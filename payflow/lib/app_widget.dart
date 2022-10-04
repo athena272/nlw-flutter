95aea084e13b72ecd074dbdb4ab7f1ac885ffdd7
athena272 <guilhermera272@gmail.com>
2022-10-04 11:44:09 -0300

    Update app_widget.dart

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -3,6 +3,7 @@ import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -13,14 +14,15 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
-        primaryColor: AppColors.primary
+        primarySwatch: Colors.orange, primaryColor: AppColors.primary,
         // primaryColor: AppColors.primary
       ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
-        "/home" : (context) => HomePage(),
-        "/login" : (context) => LoginPage()
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
