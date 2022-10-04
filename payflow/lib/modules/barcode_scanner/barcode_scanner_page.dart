63a96b29d81e5031cce38b92878a138699db0f65
athena272 <guilhermera272@gmail.com>
2022-10-04 11:44:34 -0300

    Create barcode_scanner_page.dart

---
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
+import 'package:payflow/shared/widgets/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  BarcodeScannerPage({Key? key}) : super(key: key);
+
+  @override
+  State<BarcodeScannerPage> createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escanei o código de barras",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
+            ),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(color: Colors.black.withOpacity(0.5)),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(color: Colors.transparent),
+              ),
+              Expanded(
+                child: Container(color: Colors.black.withOpacity(0.5)),
+              ),
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Insira o código",
+            primaryOnPressed: () {},
+            secondaryLabel: "Adicionar da galeria",
+            secondaryOnPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
