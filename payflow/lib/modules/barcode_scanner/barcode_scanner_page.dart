370daf334513f9c354491268c98bf26ac7c9669d
athena272 <guilhermera272@gmail.com>
2022-10-05 00:25:26 -0300

    Update barcode_scanner_page.dart

--- payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
@@ -15,6 +16,14 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
+    return BottomSheetWidget(
+      title: "Não foi possível identificar um código de barras",
+      subtitle: "Tente escanear novamente ou digite o código",
+      primaryLabel: "Esanear novamente",
+      primaryOnPressed: () {},
+      secondaryLabel: "Digitar código",
+      secondaryOnPressed: () {},
+    );
     return SafeArea(
       top: true,
       bottom: true,
@@ -53,6 +62,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
             primaryOnPressed: () {},
             secondaryLabel: "Adicionar da galeria",
             secondaryOnPressed: () {},
+            enablePrimaryColor: true,
           ),
         ),
       ),
